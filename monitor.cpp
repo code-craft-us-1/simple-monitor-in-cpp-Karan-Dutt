aad37318313b2fc93fa67d72936a9e5309915236
Karan Dutt <karan.dutt@philips.com>
2024-10-13 19:19:23 +0530

    Refactored

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,60 @@
 #include "./monitor.h"
-#include <assert.h>
 #include <thread>
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+const float MIN_TEMP = 95.0;
+const float MAX_TEMP = 102.0;
+const float MIN_PULSE = 60.0;
+const float MAX_PULSE = 100.0;
+const float MIN_SPO2 = 90.0;
+
+void displayAlert(const std::string& message) {
+    cout << message << "\n";
+    for (int i = 0; i < 6; ++i) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+}
+
+bool isTemperatureOk(float temperature)
+{
+    if (temperature < MIN_TEMP || temperature > MAX_TEMP) 
+    {
+        displayAlert("Temperature is critical!");
+        return false;
     }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    return true;
+}
+
+bool isPulseRateOk(float pulseRate)
+{
+    if (pulseRate < MIN_PULSE || pulseRate > MAX_PULSE)
+    {
+        displayAlert("Pulse Rate is out of range!");
+        return false;
     }
-    return 0;
-  }
-  return 1;
+    return true;
+}
+
+bool isSpo2Ok(float spo2)
+{
+    if (spo2 < MIN_SPO2)
+    {
+        displayAlert("Oxygen Saturation out of range!");
+        return false;
+    }
+    return true;
+}
+
+// Main function to check all vitals
+bool vitalsOk(float temperature, float pulseRate, float spo2) {
+    const bool tempOk = isTemperatureOk(temperature);
+    const bool pulseOk = isPulseRateOk(pulseRate);
+    const bool spo2Ok = isSpo2Ok(spo2);
+
+    return tempOk && pulseOk && spo2Ok;
 }
